37268c1d810c8c0f64f3db810aa8b4f49b31ed2b
Ahmad Khalilieh <ahmadfk90@gmail.com>
2018-07-07 10:52:37 +0300

    deploy

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+mongoose.connect(process.env.CONNECTION_STRING);
+
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'node_modules')));

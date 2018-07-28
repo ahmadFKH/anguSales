366011dc80b0cf200a8d821cb31784df64dc5234
Ahmad Khalilieh <ahmadfk90@gmail.com>
2018-07-28 14:32:39 +0300

    redirceting to src

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/comments', commentsRoutes);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'src/index.html'));
 });
 
 /**
@@ -36,7 +36,6 @@ app.get('*', (req, res) => {
  */
 
 const port = process.env.PORT || '3000';
-//const port = 'mysql://bee9b5de84e784:0576c4cb@us-cdbr-iron-east-04.cleardb.net/heroku_69dde057fe379a0?reconnect=true';
 app.set('port', port);
 
 app.use(function (err, req, res, next) {
